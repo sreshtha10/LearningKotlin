2223ef51863a8160dfaaf74cd8603747585bca24
Sreshtha <sreshtha.mehrotra@gmail.com>
2021-04-08 01:17:34 +0530

    Function declarations

---
+++ functions1.kt
@@ -0,0 +1,20 @@
+fun getGreeting(): String{
+	return "Hello Kotlin"    
+}
+
+
+//unit is similar means void, main function has also return type unit.
+fun sayHello(): Unit{
+    println("Say Hello")
+}
+
+// single expression function
+fun hello() = "Hello Hello"
+fun main(){
+    println("Hello")
+    println(getGreeting())
+    sayHello()
+    hello()
+}
+
+
