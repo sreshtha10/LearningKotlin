88a75358832d0395c519ac63d1886d517cd2153c
Sreshtha <sreshtha.mehrotra@gmail.com>
2021-04-08 19:07:50 +0530

    modified functions.kt

--- functions.kt
+++ functions.kt
@@ -26,3 +26,30 @@ fun main(){
 }
 */
 
+// it is possible to modify an object stored in Val
+/*fun main(){
+    val lang = mutableListOf("java")
+    lang.add("Kotlin")
+    for(x in lang){
+        println(x)
+    }
+    
+    println(max(1,3))
+}*/
+
+// prefer vals to vars
+
+
+/*
+fun max(a:Int, b:Int):Int{
+    return if(a>b) a else b
+}*/
+
+//fun max(a:Int, b:Int):Int = if(a>b) a else b
+
+// types of functions
+// top level  = fun topLevel() =1  (under the hood they are static functions same as Java)
+// member function = class A{ fun member() =2}
+// local function = fun other(){ fun local() =3}
+
+
